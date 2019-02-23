b8d848dbd512e19b1122eaf31c57456722f9ceef
Vladimir Svyatski <vsvyatski@yandex.ru>
2019-02-24 00:40:15 +0300

    Implemented action availability based on USB drive selection

--- mainwindow.py
+++ mainwindow.py
@@ -12,23 +12,30 @@ class MainWindow(QMainWindow):
         self.__ui = Ui_MainWindow()
         self.__ui.setupUi(self)
 
+        model = QStandardItemModel(self.__ui.driveList)
+        self.__ui.driveList.setModel(model)
+        self.__ui.driveList.selectionModel().selectionChanged.connect(self.__driveListSelectionChanged)
+
         self.__loadUsbDrivesIntoView()
-        self.__ui.driveList.selectionModel().selectionChanged.connect(MainWindow.__driveListSelectionChanged)
 
     def __loadUsbDrivesIntoView(self):
-        model = QStandardItemModel(self.__ui.driveList)
+        model: QStandardItemModel = self.__ui.driveList.model()
+        model.clear()
         for drive_info in driveutils.get_fat_usb_mounts():
             item = QStandardItem(QIcon(':/images/usbFlash'), drive_info.label)
             item.setData(drive_info.mount_point)
             model.appendRow(item)
 
-        self.__ui.driveList.setModel(model)
+        self.__changeActionAvailabilityBasedOnDriveSelection(False)
+
+    def __changeActionAvailabilityBasedOnDriveSelection(self, drive_selected: bool):
+        self.__ui.actionGenerate.setEnabled(drive_selected)
+        self.__ui.actionView.setEnabled(drive_selected)
 
     @pyqtSlot()
     def refreshActionTriggered(self):
         self.__loadUsbDrivesIntoView()
 
-    @pyqtSlot(QItemSelection)
-    def __driveListSelectionChanged(self, selected: QItemSelection):
-        print(selected.count())
-        print('selection changed')
+    @pyqtSlot(QItemSelection, QItemSelection)
+    def __driveListSelectionChanged(self, selected: QItemSelection, deselected: QItemSelection):
+        self.__changeActionAvailabilityBasedOnDriveSelection(not selected.isEmpty())
