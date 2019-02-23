c3a16acbcb840b5c51e5e4e3996087e44b4a463f
Vladimir Svyatski <vsvyatski@yandex.ru>
2019-02-23 22:29:43 +0300

    displaying USB drive list

--- mainwindow.py
+++ mainwindow.py
@@ -1,5 +1,8 @@
 from ui_mainwindow import Ui_MainWindow
 from PyQt5.QtWidgets import QMainWindow, QWidget
+from PyQt5.QtGui import QStandardItemModel, QStandardItem, QIcon
+import driveutils
+from PyQt5.QtCore import pyqtSlot, QItemSelection
 
 
 class MainWindow(QMainWindow):
@@ -8,3 +11,24 @@ class MainWindow(QMainWindow):
 
         self.__ui = Ui_MainWindow()
         self.__ui.setupUi(self)
+
+        self.__loadUsbDrivesIntoView()
+        self.__ui.driveList.selectionModel().selectionChanged.connect(MainWindow.__driveListSelectionChanged)
+
+    def __loadUsbDrivesIntoView(self):
+        model = QStandardItemModel(self.__ui.driveList)
+        for drive_info in driveutils.get_fat_usb_mounts():
+            item = QStandardItem(QIcon(':/images/usbFlash'), drive_info.label)
+            item.setData(drive_info.mount_point)
+            model.appendRow(item)
+
+        self.__ui.driveList.setModel(model)
+
+    @pyqtSlot()
+    def refreshActionTriggered(self):
+        self.__loadUsbDrivesIntoView()
+
+    @pyqtSlot(QItemSelection)
+    def __driveListSelectionChanged(self, selected: QItemSelection):
+        print(selected.count())
+        print('selection changed')
