f54721e83c924a493ddb1c85bbf52a134ac85cfc
Vladimir Svyatski <vsvyatski@yandex.ru>
2019-03-08 21:04:46 +0300

    Create TS files

---
+++ src/translations/kenwood_db_gen_en.ts
@@ -0,0 +1,224 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../ui_aboutdialog.py" line="87"/>
+        <source>About Kenwood Database Generator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_aboutdialog.py" line="88"/>
+        <source>Kenwood Database Generator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_aboutdialog.py" line="89"/>
+        <source>Version: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_aboutdialog.py" line="90"/>
+        <source>KMEL DB CLI version: 0.2.3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_aboutdialog.py" line="91"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;This application is basically a GUI for the kmel_db utility (&lt;a href=&quot;https://github.com/chrrrisw/kmel_db&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;https://github.com/chrrrisw/kmel_db&lt;/span&gt;&lt;/a&gt;). Its purpose is to generate a Kenwood DAP database on a selected USB drive for using that drive with Kenwood car audio. The drive must be FAT32 formatted.&lt;/p&gt;&lt;p&gt;Kenwood Database Generator is distributed under the terms of &lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.en.html&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#2980b9;&quot;&gt;GNU General Public License v3.0&lt;/span&gt;&lt;/a&gt;. &lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.py" line="198"/>
+        <source>Process finished with exit code %d
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="231"/>
+        <source>Unable to navigate to the Github repository with the default browser.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="186"/>
+        <source>Kenwood Database Generator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="187"/>
+        <source>&amp;USB Drives</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="188"/>
+        <source>Terminal &amp;Output</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="189"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="190"/>
+        <source>&amp;Drive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="191"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="192"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="193"/>
+        <source>&amp;Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="194"/>
+        <source>Refresh Drive List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="195"/>
+        <source>Ctrl+R</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="196"/>
+        <source>&amp;Write Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="197"/>
+        <source>Write Database to Drive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="198"/>
+        <source>Ctrl+W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="199"/>
+        <source>&amp;View Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="200"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="201"/>
+        <source>View Database on Drive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="202"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="203"/>
+        <source>C&amp;lear Output</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="204"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="205"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="206"/>
+        <source>&amp;Preferences...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="207"/>
+        <source>Preferences...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="208"/>
+        <source>Edit Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="209"/>
+        <source>Ctrl+,</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="210"/>
+        <source>&amp;View Repository on Github</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_mainwindow.py" line="211"/>
+        <source>View on Github</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="../preferencesdialog.py" line="46"/>
+        <source>System default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../preferencesdialog.py" line="48"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../preferencesdialog.py" line="49"/>
+        <source>Russian</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../preferencesdialog.py" line="50"/>
+        <source>Spanish</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_preferencesdialog.py" line="70"/>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_preferencesdialog.py" line="71"/>
+        <source>&amp;Select first available item on USB drives list load</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_preferencesdialog.py" line="72"/>
+        <source>S&amp;how toolbar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_preferencesdialog.py" line="73"/>
+        <source>&amp;Language*</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_preferencesdialog.py" line="74"/>
+        <source>* - this option requires restart</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
