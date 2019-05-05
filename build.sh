58f7375c117770bf1ad1d22e348633fe2ce37244
Vladimir Svyatski <vsvyatski@yandex.ru>
2019-05-06 00:09:11 +0300

    Fix homepage URL

--- build.sh
+++ build.sh
@@ -129,7 +129,7 @@ then
     cd "$debTmpDir"
 
     fpm -f -s dir -t deb -p "$outDir/../kmeldb-ui_${appVersion}_all.deb" -n kmeldb-ui -v ${appVersion} -m "$packageMaintainer" --category "utils" --deb-priority optional \
-    --license GPL-3 --vendor "Vladimir Svyatski" -a all --url https://github.com/vsvyatski/kmeldb-ui --description "$packageDescription" --deb-installed-size 6342 \
+    --license GPL-3 --vendor "Vladimir Svyatski" -a all --url https://vsvyatski.github.io/kmeldb-ui --description "$packageDescription" --deb-installed-size 6342 \
     --deb-changelog "$thisScriptDir/packaging/deb/changelog" -d "python3-pyqt5 >= 5.5~" -d "python3-venv >= 3.5~" -d "python3-wheel >= 0.29~" --deb-recommends "qttranslations5-l10n >= 5.5~" \
     --after-install "$thisScriptDir/packaging/deb/postinst" --before-remove "$thisScriptDir/packaging/deb/prerm" .
     
